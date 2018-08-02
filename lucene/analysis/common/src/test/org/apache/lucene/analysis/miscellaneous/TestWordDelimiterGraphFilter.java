b0234c07b4108ced1382507a9c71755822ed6404
Michael Gibney <michael@michaelgibney.net>
2018-08-02 14:43:16 -0400

    missing import

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/miscellaneous/TestWordDelimiterGraphFilter.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/miscellaneous/TestWordDelimiterGraphFilter.java
@@ -19,6 +19,7 @@ package org.apache.lucene.analysis.miscellaneous;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
